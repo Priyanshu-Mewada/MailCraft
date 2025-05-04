3a679dcf97764565cb78fdbfb5a901da5283613e
Priyanshu Mewada <priyanshumewada7000@gmail.com>
2025-05-05 01:00:38 +0530

    Add dist folder

---
+++ dist/src/Header.jsx
@@ -0,0 +1,51 @@
+// import React from 'react'
+
+// const Header = () => {
+//   return (
+//     <div className='bg-[#f7f7f7]'>
+//        <header className="m-2 mb-6 rounded-2xl flex justify-center items-center bg-[#333] sticky top-0 p-5 z-10 ">
+//             {/* <img 
+//             src= './logo/email.png'
+//            className="h-10 w-10"/> */}
+//             <h1 className="text-3xl font-bold text-[#fff] ">MailCraft</h1>
+//         </header>
+//     </div>
+//   )
+// }
+
+// export default Header
+
+
+
+// import React from 'react';
+
+// const Header = () => {
+//   return (
+//     <div className=" bg-[#f7f7f7]">
+//       <header className=" m-2 mb-6 rounded-2xl flex justify-center items-center bg-[#333] sticky top-0 p-5 z-50 shadow-lg">
+//         <h1 className="text-3xl font-bold text-[#fff]">MailCraft</h1>
+//       </header>
+//     </div>
+//   );
+// };
+
+// export default Header;
+
+
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='bg-[#f7f7f7]'>
+      <header className="m-1 mb-2 rounded-2xl flex justify-center items-center bg-[#333] sticky top-0 z-10 p-5">
+        {/* <img 
+            src= './logo/email.png'
+            className="h-10 w-10"
+        /> */}
+        <h1 className="text-3xl font-bold text-[#fff]">MailCraft</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
